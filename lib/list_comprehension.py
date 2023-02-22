2818c1df6f923f8202549f1fade8ec16bb46f5d8
Kyle Wehrung <kyle.wehrung@gmail.com>
2023-02-22 14:55:11 -0700

    fun stuff

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
 
+
+
 def return_evens(num_list):
-    pass
+    some_nums = [n for n in num_list if n % 2 == 0]
+    return(some_nums)
+
+return_evens([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
+
+
+
 
 def make_exclamation(sentence_list):
-    pass
+    yell = [n+"!" for n in sentence_list]
+    return yell
+make_exclamation(['I like computers', 'I require coffee', 'Live long and prosper'])
